ce763bd0baed9a8b7cee0286fd8339e45730a675
Tyson Tremblay <tremblay0995@saskpolytech.ca~>
2024-06-04 19:13:03 -0600

    done the backend portion, now need discord

--- main.py
+++ main.py
@@ -2,64 +2,96 @@ from operator import itemgetter
 import requests
 import datetime
 
-print("Hello World")
 BASE_URL = "https://api.the-odds-api.com/v4/sports"
 API_KEY = "dc2b3ae29e6afb5a4ca7ab9dbdd6d2ad"
 SPORT_NAME = "/americanfootball_cfl"
 
-def get_total_points_for_game(game):
-    for game in totals_json_data:
-        print(f"The game is {game['home_team']} vs. {game['away_team']}")
-        for bookmaker in game['bookmakers']:
-            for market in bookmaker['markets']:
-                over = market['outcomes'][0]['point']
-                under = market['outcomes'][1]['point']
-                if over == under:
-                    print(f"{bookmaker['title']} thinks that the total points are going to be {over}")
-                else:
-                    print(f"For some reason {bookmaker['title']} has different values for over({over}) and under({under})")
-    return
+def get_average_total_points_of_game(game):
+    total_points_sum = 0
 
-def get_average_home_points_dict_list(num_games, spreads_json_data):
+    # if the game does not have bookmaker data
+    if not game['bookmakers']:
+        return None
+
+    for bookmaker in game['bookmakers']:
+        for market in bookmaker['markets']:
+            over = market['outcomes'][0]['point']
+            under = market['outcomes'][1]['point']
+            if over == under:
+                total_points_sum += over
+            else:
+                raise ArithmeticError(f"For some reason {bookmaker['title']} has different values for over({over}) and under({under})")
+    average_total_points = total_points_sum / len(game['bookmakers'])
+    return average_total_points
+
+
+def get_average_home_weightings(game):
+    home_sum = 0
+
+    # if the game does not have bookmaker data
+    if not game['bookmakers']:
+        return None
+    
+    for bookmaker in game['bookmakers']:
+        # the home and away markets seem to change order, have to check
+        market1 = bookmaker['markets'][0]['outcomes'][0]
+        market2 = bookmaker['markets'][0]['outcomes'][1]
+
+        # if its the home market, add its points to the home sum
+        # else add the other markets points to the home sum
+        # hopefully there are only ever 2 markets
+        if market1['name'] == game['home_team']:
+            home_sum += market1['point']
+        else:
+            home_sum += market2['point']
+    # game_dict['total_points'] = get_total_points_for_game(spread_game)
+    if game['bookmakers']:
+        return home_sum / len(game['bookmakers'])
+    
+
+def get_dict_list(num_games):
+
+    spreads_response = requests.get(f"{BASE_URL}{SPORT_NAME}/odds?apiKey={API_KEY}&regions=uk,us,us2,eu,au&markets=spreads&date={datetime.datetime.now().isoformat}CST&all=true")
+    spreads_json_data = spreads_response.json()
+
+    totals_response = requests.get(f"{BASE_URL}{SPORT_NAME}/odds?apiKey={API_KEY}&regions=uk,us,us2,eu,au&markets=totals&date={datetime.datetime.now().isoformat}CST&all=true")
+    totals_json_data = totals_response.json()
+
+    if num_games > len(spreads_json_data):
+        num_games = len(spreads_json_data)
+
     results = []
+
 
     for nGame in range(0,num_games):
         game_dict = {}
-        game = spreads_json_data[nGame]
-        game_dict['id'] = game['id']
-        game_dict['home'] = game['home_team']
-        game_dict['away'] = game['away_team']
+        
+        spread_game = spreads_json_data[nGame]
+        totals_game = totals_json_data[nGame]
 
-        home_sum = 0
-        print(f"The game is {game_dict['home']} vs. {game_dict['away']}")
-        bookmaker_count = 0
-        for bookmaker in game['bookmakers']:
-            # the home and away markets seem to change order, have to check
-            market1 = bookmaker['markets'][0]['outcomes'][0]
-            market2 = bookmaker['markets'][0]['outcomes'][1]
+        game_dict['id'] = spread_game['id']
+        game_dict['home'] = spread_game['home_team']
+        game_dict['away'] = spread_game['away_team']
 
-            # if its the home market, add its points to the home sum
-            # else add the other markets points to the home sum
-            # hopefully there are only ever 2 markets
-            if market1['name'] == game_dict['home']:
-                home_sum += market1['point']
-            else:
-                home_sum += market2['point']
-            bookmaker_count += 1
+        game_dict['home_average'] = get_average_home_weightings(spread_game)
+        game_dict['total_points'] = get_average_total_points_of_game(totals_game)
+        
+        # if any of these are none, then don't add it to the results dict
+        if not game_dict['home_average'] or not game_dict['total_points']:
+            continue
 
-        game_dict['home_average'] = home_sum / bookmaker_count
         results.append(game_dict)
+
     return results
 
 
+# Returns a string to send as a discord message to the user
 def get_rankings(home_game_average_list):
     # [{id:2o34823, home: roughriders, away: stampeders, home_average: -5.3}, {...}]
-    print(home_game_average_list)
     new_list = sorted(home_game_average_list, key=lambda d: abs(d['home_average']))
-    print(new_list)
 
-
-
+    return_string = ""
+    # loop through original list to preserve the order by soonest game
     for game in home_game_average_list:
         game['priority'] = new_list.index(game) + 1
 
@@ -70,32 +102,22 @@ def get_rankings(home_game_average_list):
         else:
             winner = game['away']
 
-        print(f"{game['home']} vs {game['away']}\tWinner: {winner}\tPriority: {game['priority']}")
+        return_string += f"{game['home']} vs {game['away']}\tWinner: {winner}\tPriority: {game['priority']}\tGame total points: {game['total_points']}\n"
+    return return_string
 
 
-def print_insider_data(num_games):
-    spreads_response = requests.get(f"{BASE_URL}{SPORT_NAME}/odds?apiKey={API_KEY}&regions=uk,us,us2,eu,au&markets=spreads&date={datetime.datetime.now().isoformat}CST&all=true")
-    spreads_json = spreads_response.json()
-    dict_list = get_average_home_points_dict_list(num_games, spreads_json)
+# performs the whole shebang, returns the string to send to the user on discord
+def get_insider_data(num_games):
+    dict_list = get_dict_list(num_games)
+    return_string = get_rankings(dict_list)
+    return return_string
+
+
 
     
-
-
-
-
-# response = requests.get(f"{BASE_URL}?apiKey={API_KEY}&all=false")
-# response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_cfl/odds?apiKey=" + API_KEY + "&all=true")
-# response = requests.get("https://api.the-odds-api.com/v4/sports/americanfootball_cfl/odds?apiKey=dc2b3ae29e6afb5a4ca7ab9dbdd6d2ad&regions=us&markets=totals&dateFormat=iso&oddsFormat=decimal&date=2024-06-20T12%3A15%3A00CST&all=true")
-# response = requests.get(f"{BASE_URL}{SPORT_NAME}/odds?apiKey={API_KEY}&regions=uk,us,us2,eu,au&markets=totals&date={datetime.datetime.now().isoformat}CST&all=true")
-response = requests.get(f"{BASE_URL}{SPORT_NAME}/odds?apiKey={API_KEY}&regions=uk,us,us2,eu,au&markets=spreads&date={datetime.datetime.now().isoformat}CST&all=true")
-# print(response)
-json_data = response.json()
-# print(json_data)
-# print_total_point_data(json_data)
-game_points_list = get_average_home_points_dict_list(4,json_data)
+game_points_list = get_dict_list(4)
 get_rankings(game_points_list)
 
-print_insider_data(4)
-
+print(get_insider_data(10))
 
 
